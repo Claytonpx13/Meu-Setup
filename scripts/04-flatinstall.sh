8660cc799d64cad18ec0b71aefd7cfe7758d2bd0
Clayton Pereira <claytonp_13@hotmail.com>
2021-05-18 19:07:58 -0300

    Update 04-flatinstall.sh

--- scripts/04-flatinstall.sh
+++ scripts/04-flatinstall.sh
@@ -12,6 +12,9 @@
 # Licença: GPLv3 <https://www.gnu.org/licenses/gpl-3.0.txt>
 # ------------------------------------------------------------------------------
 
+apt install flatpak -y
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
 pacotes=(org.jdownloader.JDownloader)
 
 for pacote in ${pacotes[@]}
